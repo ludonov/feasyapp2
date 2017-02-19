1807bb1f386c60bf4bb40c8ce3340c56878aa89b
Daniele Pettenuzzo <danielepettenuzzo@dani-4.local>
2017-02-19 14:02:45 +0100

    editProfile

--- pages/24_edit_profile/24_edit_profile.ts
+++ pages/24_edit_profile/24_edit_profile.ts
@@ -3,6 +3,8 @@ import { Component } from '@angular/core';
 import { NavController} from 'ionic-angular';
 import { AngularFire } from 'angularfire2';
 
+import { SettingsPage } from '../../pages/23_settings/23_settings';
+
 
 @Component({
   selector: 'page-editprofile',
@@ -12,6 +14,11 @@ export class EditProfilePage {
 
   constructor(public navCtrl: NavController) {
   
+  }
+
+  changeProfile(): void {
+    console.log("edit profile");
+    this.navCtrl.push(SettingsPage);
   }
 
 
