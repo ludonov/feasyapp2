06fa2cdb1849c322e11bf5f36c05d9ef7fdca5c9
dpettenuzzo <danielepettenuzzo@dani-4.lan>
2017-02-21 19:15:22 +0100

    pagine inserimento dati dopo iscrizione

--- pages/3_signup/3_signup.ts
+++ pages/3_signup/3_signup.ts
@@ -6,6 +6,8 @@ import { FirebaseError } from 'firebase';
 
 import { FeasyUser } from '../../classes/Feasy';
 
+import { SetPersonalDetailsPage } from '../../pages/4A_set_personal_details/4A_set_personal_details';
+
 @Component({
   selector: 'page-signup',
   templateUrl: '3_signup.html'
@@ -19,6 +21,12 @@ export class SignupPage {
     console.log("NAV> signup page");
     //this.user = Backendless.UserService.login("ludovico.novelli@gmail.com", "prova", true);
     //console.log(this.user);
+  }
+
+  Test_Page4(): void {
+    console.log("personal details set");
+    //send password to users email!!!!!!
+    this.navCtrl.push(SetPersonalDetailsPage);
   }
 
   signUp(): void {
