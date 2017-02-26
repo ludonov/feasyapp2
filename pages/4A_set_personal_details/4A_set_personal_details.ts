dbf44e8fbede7e4119a7502ffe17506b6e0efc2e
Ludwig Feed <ludwig.feed@hotmail.com>
2017-02-26 14:05:43 +0100

    last fix 4A

--- pages/4A_set_personal_details/4A_set_personal_details.ts
+++ pages/4A_set_personal_details/4A_set_personal_details.ts
@@ -3,13 +3,7 @@ import { Component } from '@angular/core';
 
 import { NavController, AlertController } from 'ionic-angular';
 import { AngularFire, FirebaseObjectObservable } from 'angularfire2';
-<<<<<<< HEAD
 import { FirebaseError } from 'firebase';
-=======
-
-import { FeasyUser, FeasyList, FeasyItem, DeliveryAddress, StripForFirebase, copyObject } from '../../classes/Feasy';
-
->>>>>>> ludo
 
 import { SetAddressPage } from '../../pages/4B_set_address/4B_set_address';
 import { HomePage } from '../../pages/5_home/5_home';
@@ -23,7 +17,6 @@ import { FeasyUser, FeasyList, FeasyItem, DeliveryAddress, StripForFirebase, cop
 })
 export class SetPersonalDetailsPage {
 
-<<<<<<< HEAD
   public user: FeasyUser = new FeasyUser("", "", "");
   public user_db: FirebaseObjectObservable<any>;
 
@@ -35,32 +28,19 @@ export class SetPersonalDetailsPage {
         this.user = new FeasyUser(af.auth.getAuth().auth.email, "", "");
       }
     });
-=======
-  public user: FirebaseObjectObservable<any>;
-
-  constructor(public navCtrl: NavController, public af: AngularFire, public alertController: AlertController) {
-    this.user = af.database.object("users/" + af.auth.getAuth().uid);
->>>>>>> ludo
   }
 
   skipToHome(): void {
     console.log("skip to home");
     //this.navCtrl.push(HomePage);
-
-    this.user.set(this.user).then(res => {
-      //this.navCtrl.push(new page);
-    }).catch((err: Error) => {
-      console.log("Error: " + err.message);
-    });
-
   }
 
   setPersonalDetails(): void {
     console.log("personal details set");
     this.user_db.set(StripForFirebase(this.user)).then(res => {
-        this.navCtrl.push(SetAddressPage);
+      this.navCtrl.push(SetAddressPage);
     }).catch((err: Error) => {
-        console.log("Error: " + err.message);
+      console.log("Error: " + err.message);
     });
   }
 
