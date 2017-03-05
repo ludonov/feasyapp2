79a74ae27c0f146959e095e99a43efe6e0c3af54
Ludwig Feed <ludwig.feed@hotmail.com>
2017-03-05 15:49:48 +0100

    implementato: forgot password

--- pages/2_forgot_pass/2_forgot_pass.ts
+++ pages/2_forgot_pass/2_forgot_pass.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+﻿import { Component, Inject } from '@angular/core';
 
-import { NavController} from 'ionic-angular';
-import { AngularFire } from 'angularfire2';
+import { NavController, AlertController } from 'ionic-angular';
+import { AngularFire, FirebaseApp } from 'angularfire2';
+import * as firebase from 'firebase';
+
+import { FeasyUser } from '../../classes/Feasy';
 
 import { LoginPage } from '../../pages/1_login/1_login';
 
@@ -12,14 +15,27 @@ import { LoginPage } from '../../pages/1_login/1_login';
 })
 export class ForgotPassPage {
 
-  constructor(public navCtrl: NavController) {
-  
+  private user: FeasyUser = new FeasyUser("", "", "");
+  private fAppAuth: firebase.auth.Auth;
+
+  constructor(public navCtrl: NavController, @Inject(FirebaseApp) firebaseApp: firebase.app.App, public alertCtrl: AlertController) {
+    this.fAppAuth = firebaseApp.auth();
   }
 
   sendPassword(): void {
-    console.log("send password");
-    //send password to users email!!!!!!
-    this.navCtrl.push(LoginPage);
+    console.log("sending reset password");
+    this.fAppAuth.sendPasswordResetEmail(this.user.Email).then(() => {
+      console.log("reset password succeded");
+      this.navCtrl.pop();
+    }).catch((err: Error) => {
+      console.warn("Cannot send reset password: " + err.message);
+      let alert = this.alertCtrl.create({
+        title: 'Info',
+        subTitle: "Impossibile resettare la password: " +  err.message,
+        buttons: ['Ok']
+      });
+      alert.present();
+    });
   }
 
 }
