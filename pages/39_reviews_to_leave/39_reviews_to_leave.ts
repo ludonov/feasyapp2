c5897b013d4f1e262c47f2ac281f3cd88d31e235
dpettenuzzo <danielepettenuzzo@dani-4.local>
2017-04-24 14:09:06 +0200

    reviewsToLeave_pt3

--- pages/39_reviews_to_leave/39_reviews_to_leave.ts
+++ pages/39_reviews_to_leave/39_reviews_to_leave.ts
@@ -15,37 +15,31 @@ import { Globals } from '../../classes/Globals';
 
 export class ReviewsToLeavePage {
 
-
     public ReviewsToLeaveAsDemander: Array<any> = new Array<any>();
     public ReviewsToLeaveAsShopper: Array<any> = new Array<any>();
-    public TerminatedListsAsDemander_db: FirebaseListObservable<any>;
-    public TerminatedListsAsShopper_db: FirebaseListObservable<any>;
+
+    public TerminatedLists_db: FirebaseListObservable<any>; 
     public TerminatedListsAsDemander: Object = {};
     public TerminatedListsAsShopper: Object = {};
 
     constructor(public cd: ChangeDetectorRef, public navCtrl: NavController, public globals: Globals, public af: AngularFire, public alertCtrl: AlertController) {
-        this.TerminatedListsAsDemander_db = af.database.list("terminated_lists/" + globals.UID + "/as_demander"); 
-        this.TerminatedListsAsDemander_db.$ref.on("value", (snapshot1: firebase.database.DataSnapshot) => {
-            this.TerminatedListsAsDemander=snapshot1.val();
+        this.TerminatedLists_db = af.database.list("terminated_lists/" + globals.UID); 
+        this.TerminatedLists_db.$ref.on("value", (snapshot: firebase.database.DataSnapshot) => {
+            let lists: any = snapshot.val();
+            this.TerminatedListsAsDemander=lists["as_demander"];
+            this.TerminatedListsAsShopper=lists["as_shopper"];
+
+            for (let demander_key in this.TerminatedListsAsDemander) {
+                let list_d: FeasyList = this.TerminatedListsAsDemander[demander_key];
+                if(list_d.ReviewDone == false)
+                    this.ReviewsToLeaveAsDemander.push(list_d);
+            }
+
+            for (let shopper_key in this.TerminatedListsAsShopper) {
+                let list_s: FeasyList = this.TerminatedListsAsShopper[shopper_key];
+                if(list_s.ReviewDone == false)
+                    this.ReviewsToLeaveAsShopper.push(list_s);
+            }
         });
-
-        this.TerminatedListsAsShopper_db = af.database.list("terminated_lists/" + globals.UID + "/as_shopper"); 
-        this.TerminatedListsAsShopper_db.$ref.on("value", (snapshot2: firebase.database.DataSnapshot) => {
-            this.TerminatedListsAsShopper=snapshot2.val();
-        });
-
-        for (let demander_key in this.TerminatedListsAsDemander) {
-            let list_d: FeasyList = this.TerminatedListsAsDemander[demander_key];
-            if(list_d.ReviewDone == false)
-                this.ReviewsToLeaveAsDemander.push(list_d);
-        }
-
-        for (let shopper_key in this.TerminatedListsAsShopper) {
-            let list_s: FeasyList = this.TerminatedListsAsShopper[shopper_key];
-            if(list_s.ReviewDone == false)
-                this.ReviewsToLeaveAsShopper.push(list_s);
-        }
-
     }
-
 }
